67d218011e9c06b062ffb361d5d3eab3dac5bc29
ivanzolotov <ivanzolotov@ivanzolotov.ru>
2019-12-13 20:10:23 +0500

    Change codestyle

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-const TASK_COUNT = 3;
-
 import {createSiteMenuTemplate} from './components/site-menu.js';
 import {createFilterTemplate} from './components/filter.js';
 import {createBoardTemplate} from './components/board.js';
@@ -7,13 +5,14 @@ import {createTaskTemplate} from './components/task.js';
 import {createTaskEditTemplate} from './components/task-edit.js';
 import {createLoadMoreButtonTemplate} from './components/load-more.js';
 
+const TASK_COUNT = 3;
+
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
-
 render(siteHeaderElement, createSiteMenuTemplate());
 render(siteMainElement, createFilterTemplate());
 render(siteMainElement, createBoardTemplate());
@@ -24,13 +23,6 @@ render(taskListElement, createTaskEditTemplate());
 for (let i = 0; i < TASK_COUNT; i++) {
   render(taskListElement, createTaskTemplate());
 }
-
-// В лекции итерировались вот так, но зачем?!
-// new Array(TASK_COUNT)
-//   .fill(``)
-//   .forEach(
-//     () => render(taskListElement, createTaskTemplate())
-//   );
 
 const boardElement = siteMainElement.querySelector(`.board`);
 render(boardElement, createLoadMoreButtonTemplate());
