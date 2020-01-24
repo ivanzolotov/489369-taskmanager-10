8e683500d3d5169a230adbcf691462d647f1168d
ivanzolotov <ivanzolotov@ivanzolotov.ru>
2020-01-25 01:23:57 +0500

    Add unit 9 functionality

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import {render} from './utils.js';
 import SiteMenuComponent from './components/site-menu.js';
 import FilterComponent from './components/filter.js';
 import BoardComponent from './components/board.js';
+import SortComponent from './components/sort.js';
+import TasksComponent from './components/tasks.js';
+import NoTasksComponent from './components/no-tasks.js';
 import TaskComponent from './components/task.js';
 import TaskEditComponent from './components/task-edit.js';
 import LoadMoreButtonComponent from './components/load-more.js';
@@ -23,18 +26,31 @@ const removePointlessLoadMoreButton = (component, showedTasks, loadedTasks) => {
 
 const filters = generateFilters();
 const tasks = generateTasks(TASK_COUNT);
+const isAllTasksArchived = tasks.every((task) => task.isArchive);
 
 const renderTask = (task) => {
   let taskComponent = new TaskComponent(task);
-  let taskEditComponent = new TaskEditComponent(task);
   const editButton = taskComponent.getElement().querySelector(`.card__btn--edit`);
-  editButton.addEventListener(`click`, () => {
-    taskListElement.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
-  });
+
+  let taskEditComponent = new TaskEditComponent(task);
   const editForm = taskEditComponent.getElement().querySelector(`form`);
+
+  editButton.addEventListener(`click`, () => {
+    const onEscKeyDown = (event) => {
+      if (event.keyCode === 27) {
+        taskListElement.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    taskListElement.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
+    document.addEventListener('keydown', onEscKeyDown);
+  });
+
   editForm.addEventListener(`submit`, () => {
     taskListElement.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
   });
+
   render(taskListElement, taskComponent.getElement(), `beforeend`);
 };
 
@@ -44,7 +60,17 @@ render(siteHeaderElement, new SiteMenuComponent().getElement(), `beforeend`);
 render(siteMainElement, new FilterComponent(filters).getElement(), `beforeend`);
 
 const boardComponent = new BoardComponent();
+const sortComponent = new SortComponent();
+const tasksComponent = new TasksComponent();
+
 render(siteMainElement, boardComponent.getElement(), `beforeend`);
+
+if (isAllTasksArchived) {
+  render(boardComponent.getElement(), new NoTasksComponent().getElement(), `beforeend`);
+} else {
+  render(boardComponent.getElement(), sortComponent.getElement(), `beforeend`);
+  render(boardComponent.getElement(), tasksComponent.getElement(), `beforeend`);
+}
 
 const taskListElement = boardComponent.getElement().querySelector(`.board__tasks`);
 
